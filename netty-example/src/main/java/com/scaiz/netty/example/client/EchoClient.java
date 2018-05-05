b5280f4850d1b36573e66f7f7a5fa39a187cf3e6
zsc347 <zsc347@163.com>
2018-05-05 22:22:40 +0800

    impl of simple echo client and echo server by netty

---
+++ netty-example/src/main/java/com/scaiz/netty/example/client/EchoClient.java
@@ -0,0 +1,66 @@
+package com.scaiz.netty.example.client;
+
+import com.scaiz.netty.example.client.handler.EchoClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+public final class EchoClient {
+
+  private static final boolean SSL = System.getProperty("ssl") != null;
+  private static final String HOST = System.getProperty("host", "127.0.0.1");
+  private static final int PORT = Integer
+      .parseInt(System.getProperty("port", "8000"));
+
+  private void start() throws Exception {
+    // Configure SSL.git
+    final SslContext sslCtx;
+    if (SSL) {
+      sslCtx = SslContextBuilder.forClient()
+          .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+    } else {
+      sslCtx = null;
+    }
+
+    // Configure the client.
+    EventLoopGroup group = new NioEventLoopGroup();
+    try {
+      Bootstrap b = new Bootstrap();
+      b.group(group)
+          .channel(NioSocketChannel.class)
+          .option(ChannelOption.TCP_NODELAY, true)
+          .handler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) {
+              ChannelPipeline p = ch.pipeline();
+              if (sslCtx != null) {
+                p.addLast(sslCtx.newHandler(ch.alloc(), HOST, PORT));
+              }
+              p.addLast(new EchoClientHandler());
+            }
+          });
+
+      // Start the client.
+      ChannelFuture f = b.connect(HOST, PORT).sync();
+
+      // Wait until the connection is closed.
+      f.channel().closeFuture().sync();
+    } finally {
+      // Shut down the event loop to terminate all threads.
+      group.shutdownGracefully();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    new EchoClient().start();
+  }
+}
---
+++ netty-example/src/main/java/com/scaiz/netty/example/client/handler/EchoClientHandler.java
@@ -0,0 +1,26 @@
+package com.scaiz.netty.example.client.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) {
+    ctx.writeAndFlush(Unpooled.copiedBuffer("Hi, Netty", CharsetUtil.UTF_8));
+  }
+
+  @Override
+  protected void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
+    System.out.println("Client Received: " + in.toString(CharsetUtil.UTF_8));
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
